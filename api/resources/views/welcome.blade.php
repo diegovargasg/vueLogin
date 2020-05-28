987edf733f0794aede425540e37f16c76e4f3297
Diego <diegocvg@gmail.com>
2020-05-28 10:25:30 +0200

    updated composer.lock

--- api/resources/views/welcome.blade.php
+++ api/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel API TEST
                 </div>
 
                 <div class="links">
